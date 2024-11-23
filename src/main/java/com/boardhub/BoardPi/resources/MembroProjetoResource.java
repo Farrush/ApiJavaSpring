b7a2010ec0d41743dc8cfd1398ec526856b9ce82
RAFAEL CARRENHO TARANTO <rcarrenhot@gmail.com>
2024-11-23 16:14:42 -0300

    rota de busca de membro por projeto

--- src/main/java/com/boardhub/BoardPi/resources/MembroProjetoResource.java
+++ src/main/java/com/boardhub/BoardPi/resources/MembroProjetoResource.java
@@ -1,6 +1,7 @@
 package com.boardhub.BoardPi.resources;
 
 import com.boardhub.BoardPi.entities.MembroProjeto;
+import com.boardhub.BoardPi.entities.Projeto;
 import com.boardhub.BoardPi.services.MembroProjetoService;
 import com.boardhub.BoardPi.services.ProjetoService;
 import com.boardhub.BoardPi.services.UsuarioService;
@@ -31,7 +32,11 @@ public class MembroProjetoResource {
     public MembroProjeto buscaPorId(@PathVariable long id) {
         return membroProjetoService.getMembroProjeto(id);
     }
-
+    @RequestMapping(value = "/projeto/{id}")
+    public List<MembroProjeto> buscaPorProjeto(@PathVariable long id) {
+        Projeto p = projetoService.findById(id);
+        return membroProjetoService.getPorProjeto(p);
+    }
     @RequestMapping(value = "/membro/{idMembro}/projeto/{idProjeto}", method = RequestMethod.POST)
     public MembroProjeto adicionar(@PathVariable long idMembro, @PathVariable long idProjeto, @RequestBody MembroProjeto membroProjeto) {
         membroProjeto.setProjeto(projetoService.findById(idProjeto));
